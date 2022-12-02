ddb144b8b1bab36f509c9fcb25f9a4af2e12ff09
Edward Leong <edwaleong@gmail.com>
2022-12-02 16:18:47 -0500

    more update

---
+++ api/index.js
@@ -0,0 +1,41 @@
+// Pinboard API proxy. 
+// For whatever reason the Pinboard API (https://pinboard.in/howto/#api) doesn't 
+// have CORS enabled — so you can't make API calls directly from the browser. 
+// This small server proxies requests to the Pinboard API allowing CORS. 
+
+// Create a proxy to redirect requests of the "/api/*" path to the Pinboard API.
+// Examples:
+// GET /api/v1/posts/all?format=json → https://api.pinboard.in/v1/posts/all?format=json
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import NextCors  from 'nextjs-cors';
+
+const apiProxy = createProxyMiddleware({
+  target: "https://m2devpwa.ingredientsonline.com",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/api": "/graphql" // Strip "/api" from the URL 
+  },  
+  onProxyRes(proxyRes) {
+    proxyRes.headers["access-control-allow-origin"] = "*", 
+    proxyRes.headers["access-control-allow-methods"] = "DELETE, POST, GET, OPTIONS, PUT, PATCH",
+    proxyRes.headers["access-control-allow-headers"] = "Origin, X-Requested-With, Content-Type, Accept" 
+  }
+});
+
+// In Vercel, any file inside the "api" directory is mapped to "/api" and
+// will be treated as an API endpoint.
+// By default, on Vercel this "/api" endpoint would strictly match only "/api" 
+// requestes (ignoring sub-paths like "/api/hello"). So, to proxy the entire 
+// path, we add a rewrite in "vercel.json" to allow the "api" directory to catch
+// all "/api/*" requests.
+export default async function (req, res) {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+  // Proxy "/api/*" requests to the pinboard API.
+  return apiProxy(req, res);
+};
+
+
